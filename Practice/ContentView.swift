57e04ed0075d7c7e3a3d3117f72ac37d0768d6cc
Saba Karbassi <skarbassi2005@gmail.com>
2023-07-13 14:01:48 -0700

    GitHub practice

--- Practice/ContentView.swift
+++ Practice/ContentView.swift
@@ -15,7 +15,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             
-            
+        Text("GitHub")
             Text(textTitle)
                 .font(.title)
             
